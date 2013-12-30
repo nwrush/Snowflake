8793a96b87b52d0749ea8af714cecf3e0cb48a95
p0rtalplayer <subject.johnson@gmail.com>
2013-12-29 23:05:06 -0800

    Transition to mogre: converted axiom things to mogre things

---
+++ GV.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Mogre;
+
+namespace Snowflake {
+    public class GV {
+        public static Root Root;
+        public static RenderWindow Window;
+        public static SceneManager SceneManager;
+        public static OverlayManager OverlayManager;
+        public static MOIS.InputManager inputManager;
+        public static MOIS.Keyboard inputKeyboard;
+        public static MOIS.Mouse inputMouse;
+    }
+}

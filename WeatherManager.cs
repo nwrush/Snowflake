886578edd7239e8bd086304dee948d1c75e16ec5
Andrew <subject.johnson@gmail.com>
2013-12-30 15:48:45 -0800

    Created WeatherManager - nonfunctional as of yet

---
+++ WeatherManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snowflake {
+    public class WeatherManager {
+
+        public List<Weather> PastWeather;
+        public Weather CurrentWeather;
+        public Weather NextWeather;
+
+        private int timer;
+
+        public void Update() {
+            timer -= 1;
+        }
+
+        public void SwitchWeather(Weather w) {
+            PastWeather.Add(w);
+            CurrentWeather = w;
+
+        }
+    }
+
+    public enum Weather {
+        Null,
+        Sunny,
+        Cloudy,
+        Rainy,
+        Stormy,
+        Windy,
+        Foggy
+    }
+}

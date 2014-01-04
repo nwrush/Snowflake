1d115821123973e219f7481b1f9357dea3e4c3cd
Nikko <nikko.rush@gmail.com>
2014-01-03 23:46:17 -0800

    I need to think about this

---
+++ Dreaded Rapid Toothbrush/City.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DRT {
+    public class City {
+        private Tuple<int, int> size;
+        private string name;
+        private int population;
+    }
+}

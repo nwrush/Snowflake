0497232bfa2b3a7017eb5d4561e71e7beba878a8
Nikko <nikko.rush@gmail.com>
2014-01-04 17:49:02 -0800

    Why?

---
+++ Snowflake/ResidentialBuilding.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DRT.Buildings {
+    public class ResidentialBuilding : Building {
+        public ResidenceType Type;
+    }
+
+    public enum ResidenceType {
+        House,
+        Apartment
+    }
+}

8537a744a97fb5a22597c8ab697dc8863a8ce86a
p0rtalplayer <subject.johnson@gmail.com>
2014-01-15 22:09:13 -0800

    Organized resources, got selection box working + nice looking

--- Snowflake/Buildings/ParkBuilding.cs
+++ Snowflake/Buildings/ParkBuilding.cs
@@ -8,13 +8,12 @@ namespace Snowflake.Buildings {
     public class ParkBuilding : Building {
 
         public override void CreateEntities(SceneManager sm) {
-            this.entities.Add(sm.CreateEntity(this.GetHashCode().ToString(), "park.mesh"));
-            this.entities[0].SetMaterialName("Grass");
-            this.entities[0].CastShadows = true;
+            this.entities.Add(sm.CreateEntity(this.GetHashCode().ToString(), "sel_Box001.mesh"));
+            this.entities[0].CastShadows = false;
         }
         public override void AttachNode(Mogre.SceneManager sm) {
             base.AttachNode(sm);
-            this.node.SetScale(new Mogre.Vector3(40, 40, 40));
+            this.node.SetScale(new Mogre.Vector3(420, 420, 420));
         }
     }
 }

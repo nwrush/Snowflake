716cb88d0e40f212d57bcd89b27f118e8975ebe8
nikko.rush <nikko.rush@gmail.com>
2014-01-23 09:42:29 -0800

    EULA and Money

---
+++ Haswell/Exceptions/NotEnoughMoneyException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Haswell.Exceptions {
+    [Serializable]
+    public class NotEnoughMoneyException : Exception {
+        public NotEnoughMoneyException() { }
+        public NotEnoughMoneyException(string message) : base(message) { }
+        public NotEnoughMoneyException(string message, Exception inner) : base(message, inner) { }
+        protected NotEnoughMoneyException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}

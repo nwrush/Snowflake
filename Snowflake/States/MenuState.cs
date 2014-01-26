e3d6ee10eae0df372061131692477e651d23110f
p0rtalplayer <subject.johnson@gmail.com>
2014-01-25 17:45:30 -0800

    Added framework for Menu state

---
+++ Snowflake/States/MenuState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Mogre;
+
+using Miyagi;
+using Miyagi.Common;
+using Miyagi.Common.Data;
+using Miyagi.UI;
+using Miyagi.UI.Controls;
+
+using Snowflake.GuiComponents;
+using Snowflake.Modules;
+
+namespace Snowflake.States {
+    public class MenuState : State {
+
+        public MenuState() {
+            
+        }
+
+        public override bool Startup(StateManager _mgr) {
+
+            return true;
+        }
+
+        public override void Update(float _frameTime) {
+
+        }
+
+        public override void Shutdown() {
+            
+        }
+    }
+}

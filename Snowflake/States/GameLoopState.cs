910c1e26808868459d12caf7d3c06b7adc70f512
p0rtalplayer <subject.johnson@gmail.com>
2014-01-28 18:40:25 -0800

    Refactor GameLoopState into partial classes, add logo to icon sheet

--- Snowflake/States/GameLoopState.cs
+++ Snowflake/States/GameLoopState.cs
@@ -20,7 +20,7 @@ namespace Snowflake.States {
     /// <summary>
     /// Program state for playing the game
     /// </summary>
-    public class GameLoopState : State {
+    public partial class GameLoopState : State {
 
         public StateManager StateMgr { get; private set; }
         private Environment WeatherMgr;
@@ -43,6 +43,7 @@ namespace Snowflake.States {
         private DebugPanel DebugPanel;
         private ContextMenu ContextMenu;
 
+        //Scale factor to get from 20 3ds max units to 1 plot grid square (120 mogre units)
         private const float SCALEFACTOR = 473.0f;
 
         /// <summary>
@@ -228,182 +229,6 @@ namespace Snowflake.States {
             CompositorManager.Singleton.RemoveAll();
             CompositorManager.Singleton.UnloadAll();
             CompositorManager.Singleton.Dispose();
-        }
-
-        /// <summary>
-        /// Update the game
-        /// </summary>
-        /// <param name="_frameTime"></param>
-        public override void Update(float _frameTime) {
-            // check if the state was initialized before
-            if (StateMgr == null)
-                return;
-
-            UpdateCameraPosition();
-            HandleInput(StateMgr);
-
-            if (CityManager.Initialized) {
-                CityManager.Update(_frameTime);
-                UpdateGUI(_frameTime);
-            }
-
-            WeatherMgr.Update(StateMgr.Engine.SceneMgr);
-            DebugPanel.UpdateFPS(_frameTime);
-        }
-
-        private void UpdateCameraPosition() {
-            StateMgr.Engine.Camera.Position = new Vector3(focalPoint.Position.x + (-500) * Mogre.Math.Cos(angle), 500, focalPoint.Position.z + -(500) * Mogre.Math.Sin(angle));
-            StateMgr.Engine.Camera.Position += StateMgr.Engine.Camera.Direction * (float)System.Math.Pow(dist, 3);
-        }
-
-        /// <summary>
-        /// Provide input handling during the game.
-        /// </summary>
-        /// <param name="mStateMgr"></param>
-        private void HandleInput(StateManager mStateMgr) {
-            // get reference to the ogre manager
-            OgreManager engine = mStateMgr.Engine;
-
-            //If we're not typing into a form or something...
-            if (!StateManager.SupressGameControl) {
-                Ray mouseRay = GetSelectionRay(mStateMgr.Input.MousePosX, mStateMgr.Input.MousePosY);
-
-                Mogre.Pair<bool, float> intersection = mouseRay.Intersects(new Plane(Vector3.UNIT_Y, Vector3.ZERO));
-                if (intersection.first && selboxShouldUpate()) {
-                    Vector3 intersectionPt = mouseRay.Origin + mouseRay.Direction * intersection.second;
-                    Vector3 plotCenter = CityManager.GetPlotCenter(CityManager.GetPlotCoords(intersectionPt));
-                    cursorPlane.SetPosition(plotCenter.x, plotCenter.y + 1f, plotCenter.z);
-                    DebugPanel.SetDebugText(CityManager.GetPlotCoords(intersectionPt).ToString());
-                }
-                
-                //Middle click - rotate the view
-                if (mStateMgr.Input.IsMouseButtonDown(MouseButtonID.MB_Middle) && viewShouldUpdate()) {
-
-                    //Mouse rotate control
-                    angle += mStateMgr.Input.MouseMoveX * 0.01f;
-                    //mStateMgr.Input += mStateMgr.Input.MouseMoveX;
-
-                    //Mouse drag control
-                    /*Vector2 mouseMoveRotated = Utils3D.RotateVector2(new Vector2(mStateMgr.Input.MouseMoveX, mStateMgr.Input.MouseMoveY), angle);
-                    focalPoint.Translate(new Vector3(mouseMoveRotated.y, 0, mouseMoveRotated.x));
-                    mStateMgr.GuiSystem.GUIManager.Cursor.SetActiveMode(CursorMode.ResizeTop);*/
-
-                    if (!ContextMenu.HitTest(MousePosition(mStateMgr.Input))) {
-                        ContextMenu.Visible = false;
-                    }
-                }
-                //Right click - context menus
-                if (mStateMgr.Input.WasMouseButtonPressed(MouseButtonID.MB_Right))
-                {
-                    if (ContextMenu.Visible == true && !ContextMenu.HitTest(MousePosition(mStateMgr.Input))) {
-                        ContextMenu.Visible = false;
-                    }
-                    else {
-                        ContextMenu.Location = new Point(mStateMgr.Input.MousePosX, mStateMgr.Input.MousePosY);
-                        ContextMenu.Visible = true;
-                    }
-                }
-
-                //Mouse click - 3D selection
-                if (mStateMgr.Input.WasMouseButtonPressed(MouseButtonID.MB_Left)) {
-                    if (!ContextMenu.HitTest(MousePosition(mStateMgr.Input))) {
-                        ContextMenu.Visible = false;
-
-                        if (canSelect()) {
-                            Mogre.Pair<bool, Point> result = getPlotCoordsFromScreenPoint(MousePosition(mStateMgr.Input));
-                            if (result.first) {
-                                CityManager.SetSelectionOrigin(result.second);
-                            }
-                        }
-                    }
-                }
-
-                if (mStateMgr.Input.IsMouseButtonDown(MouseButtonID.MB_Left)) {
-                   if (canSelect()) {
-                       Mogre.Pair<bool, Point> result = getPlotCoordsFromScreenPoint(MousePosition(mStateMgr.Input));
-                       if (result.first) {
-                           CityManager.UpdateSelectionBox(result.second);
-                           UpdateSelectionBox();
-                       }
-                   }
-                }
-
-                if (mStateMgr.Input.WasMouseButtonReleased(MouseButtonID.MB_Left)) {
-                    if (canSelect()) {
-                        Mogre.Pair<bool, Point> result = getPlotCoordsFromScreenPoint(MousePosition(mStateMgr.Input));
-                        if (result.first) {
-                            CityManager.UpdateSelectionBox(result.second);
-                        }
-                        CityManager.MakeSelection();
-                        UpdateSelectionBox();
-                    }
-                }
-
-                if (mStateMgr.Input.MouseMoveZ != 0.0f) {
-                    dist += mStateMgr.Input.MouseMoveZ * 0.002f;
-                    if (dist < -12.0f) { dist = -12.0f; }
-                    if (dist > 2.0f) { dist = 2.0f; }
-                }
-
-                if (viewShouldUpdate()) {
-                    //WASD Control
-                    int speed = 10;
-                    if (mStateMgr.Input.IsKeyDown(KeyCode.KC_A)) {
-                        focalPoint.Translate(new Vector3((-dist + speed) * Mogre.Math.Sin(angle), 0, -(-dist + speed) * Mogre.Math.Cos(angle)));
-                    }
-                    if (mStateMgr.Input.IsKeyDown(KeyCode.KC_W)) {
-                        focalPoint.Translate(new Vector3((-dist + speed) * Mogre.Math.Cos(angle), 0, (-dist + speed) * Mogre.Math.Sin(angle)));
-                    }
-                    if (mStateMgr.Input.IsKeyDown(KeyCode.KC_D)) {
-                        focalPoint.Translate(new Vector3(-(-dist + speed) * Mogre.Math.Sin(angle), 0, (-dist + speed) * Mogre.Math.Cos(angle)));
-                    }
-                    if (mStateMgr.Input.IsKeyDown(KeyCode.KC_S)) {
-                        focalPoint.Translate(new Vector3(-(-dist + speed) * Mogre.Math.Cos(angle), 0, -(-dist + speed) * Mogre.Math.Sin(angle)));
-                    }
-                    if (mStateMgr.Input.IsKeyDown(KeyCode.KC_Q)) {
-                        angle += 0.01f;
-                    }
-                    if (mStateMgr.Input.IsKeyDown(KeyCode.KC_E)) {
-                        angle -= 0.01f;
-                    }
-                }
-
-                //Toggle the console with `
-                if (mStateMgr.Input.WasKeyPressed(KeyCode.KC_GRAVE)) {
-                    gConsole.Visible = !gConsole.Visible;
-                }
-            }
-
-            // check if the escape key was pressed
-            if (mStateMgr.Input.WasKeyPressed(KeyCode.KC_EQUALS)) {
-                // quit the application
-                mStateMgr.RequestShutdown();
-            }
-        }
-
-        private void UpdateSelectionBox() {
-            Vector3 center = (CityManager.GetPlotCenter(CityManager.SelectionBox.Left, CityManager.SelectionBox.Top)
-                + CityManager.GetPlotCenter(CityManager.SelectionBox.Right, CityManager.SelectionBox.Bottom))
-                 * 0.5f;
-            selectionBox.SetPosition(center.x, center.y, center.z);
-            selectionBox.SetScale(CityManager.SelectionBox.Width * SCALEFACTOR + SCALEFACTOR, SCALEFACTOR / 2.0f, CityManager.SelectionBox.Height * SCALEFACTOR + SCALEFACTOR);
-            selectionBox.SetVisible(true);
-        }
-        public void UpdateGUI(float frametime) {
-            if (CityManager.Initialized)
-                Tools.Update(frametime);
-        }
-
-        private bool selboxShouldUpate() {
-            return ContextMenu.Visible == false;
-        }
-
-        private bool viewShouldUpdate() {
-            return ContextMenu.Visible == false;
-        }
-
-        private bool canSelect() {
-            return ContextMenu.Visible == false && gConsole.HitTest(MousePosition(StateMgr.Input)) == false;
         }
 
         /// <summary>
--- Snowflake/States/MenuState.cs
+++ Snowflake/States/MenuState.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 
-using Mogre;
-
 using Miyagi;
 using Miyagi.Common;
 using Miyagi.Common.Data;
@@ -14,13 +12,40 @@ using Snowflake.Modules;
 namespace Snowflake.States {
     public class MenuState : State {
 
+        private StateManager StateMgr;
+
         public MenuState() {
-            
+            StateMgr = null;
         }
 
         public override bool Startup(StateManager _mgr) {
-
+            StateMgr = _mgr;
+            CreateGui();
             return true;
+        }
+
+        private void CreateGui()
+        {
+            float gw = StateMgr.Engine.Camera.Viewport.Width;
+            float gh = StateMgr.Engine.Camera.Viewport.Height;
+
+            GUI gui = new GUI();
+            StateMgr.GuiSystem.GUIManager.GUIs.Add(gui);
+
+            Size logosize = new Size((int)(Math.Min(512, gw - 200)), (int)Math.Min(256, (gw - 200) / 2.0));
+            Panel logo = new Panel("menu_logo")
+            {
+                Skin = ResourceManager.Skins["Logo"],
+                Size = logosize,
+                Location = new Point((int)((gw - logosize.Width) / 2), 100),
+                ResizeMode = ResizeModes.None,
+                Throwable = false,
+                Movable = false,
+                BorderStyle = new Miyagi.UI.Controls.Styles.BorderStyle()
+                {
+                    Thickness = new Thickness(0, 1, 0, 1)
+                }
+            };
         }
 
         public override void Update(float _frameTime) {

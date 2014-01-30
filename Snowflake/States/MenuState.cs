d46a79cd09c7efddc1aba78b279eabdafb339619
p0rtalplayer <subject.johnson@gmail.com>
2014-01-29 17:55:27 -0800

    Added new game button

--- Snowflake/States/MenuState.cs
+++ Snowflake/States/MenuState.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-
 using Miyagi;
 using Miyagi.Common;
 using Miyagi.Common.Data;
+using Miyagi.Common.Events;
 using Miyagi.UI;
 using Miyagi.UI.Controls;
-
+using Miyagi.UI.Controls.Styles;
 using Snowflake.GuiComponents;
 using Snowflake.Modules;
-
 using Vector3 = Mogre.Vector3;
 
 namespace Snowflake.States {
@@ -29,7 +28,7 @@ namespace Snowflake.States {
             CreateGui();
             setupCamera(_mgr.Engine);
             createLight(_mgr.Engine.SceneMgr);
-            _mgr.Engine.SceneMgr.SetSkyBox(true, "Sky01", 2048, false);
+            _mgr.Engine.SceneMgr.SetSkyBox(true, "Sky01", 8192, false);
             focalPoint.AttachObject(_mgr.Engine.SceneMgr.CreateEntity(Mogre.SceneManager.PrefabType.PT_CUBE));
 
             return true;
@@ -66,21 +65,47 @@ namespace Snowflake.States {
             };
             logobg.SetBackgroundTexture(ResourceManager.Skins["BlackPanelSkin"].SubSkins["BlackPanelSkin40"]);
 
+            Button newGameButton = new Button("menu_btnNewGame") {
+                Text = "New Game",
+                Size = new Size(gw, 32),
+                Location = new Point(0, (int)(gh * 0.5f)),
+                TextStyle = new TextStyle() {
+                    ForegroundColour = Colours.White,
+                    Alignment = Alignment.MiddleCenter
+                },
+                Skin = ResourceManager.Skins["BlackPanelSkin"],
+                BorderStyle = new Miyagi.UI.Controls.Styles.BorderStyle() {
+                    Thickness = new Thickness(0, 1, 0, 1)
+                }
+            };
+            newGameButton.SetBackgroundTexture(ResourceManager.Skins["BlackPanelSkin"].SubSkins["BlackPanelSkin40"]);
+
+            newGameButton.MouseEnter += (object sender, MouseEventArgs e) => { 
+                newGameButton.SetBackgroundTexture(ResourceManager.Skins["BlackPanelSkin"].SubSkins["BlackPanelSkin60"]);
+            };
+            newGameButton.MouseLeave += (object sender, MouseEventArgs e) => {
+                newGameButton.SetBackgroundTexture(ResourceManager.Skins["BlackPanelSkin"].SubSkins["BlackPanelSkin40"]);
+            };
+            newGameButton.MouseClick += (object sender, MouseButtonEventArgs e) => {
+                StartNewGame();
+            };
+
             gui.Controls.Add(logo);
             gui.Controls.Add(logobg);
+            gui.Controls.Add(newGameButton);
         }
 
         private void setupCamera(OgreManager engine) {
-            focalPoint = engine.SceneMgr.RootSceneNode.CreateChildSceneNode("focalPoint");
+            focalPoint = engine.SceneMgr.RootSceneNode.CreateChildSceneNode("menuFocalPoint");
             focalPoint.Position = new Vector3(0, -5, 0);
 
             engine.Camera.NearClipDistance = 5;
-            engine.Camera.FarClipDistance = 4096;
+            engine.Camera.FarClipDistance = 16384;
             engine.Camera.AutoAspectRatio = true;
             engine.Camera.SetAutoTracking(true, focalPoint);
         }
         private void createLight(Mogre.SceneManager sm) {
-            Mogre.Light ambient = sm.CreateLight("ambient");
+            Mogre.Light ambient = sm.CreateLight("menuAmbient");
             ambient.Type = Mogre.Light.LightTypes.LT_DIRECTIONAL;
             ambient.Position = new Vector3(0, 2000, 0);
             ambient.Direction = new Vector3(-0.8365f, -1, 0.5867f);
@@ -98,6 +123,12 @@ namespace Snowflake.States {
         private void UpdateCameraPosition() {
             StateMgr.Engine.Camera.Position = new Vector3(focalPoint.Position.x + (-500) * Mogre.Math.Cos(angle), 100 * (float)System.Math.Sin(angle), focalPoint.Position.z + -(500) * Mogre.Math.Sin(angle));
             StateMgr.Engine.Camera.Position += StateMgr.Engine.Camera.Direction * (float)System.Math.Pow(dist, 3);
+        }
+
+        public void StartNewGame() {
+            StateMgr.Engine.SceneMgr.RootSceneNode.RemoveAndDestroyAllChildren();
+            StateMgr.GuiSystem.GUIManager.DisposeAllGUIs();
+            StateMgr.RequestStateChange(typeof(GameLoopState));
         }
 
         public override void Shutdown() {

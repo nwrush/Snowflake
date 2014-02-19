62a888ae10b5f20f76072488d0a32d1278849c9c
Andrew <subject.johnson@gmail.com>
2014-02-19 15:13:32 -0800

    Created class for expander panel

---
+++ Snowflake/GuiComponents/ExpanderPanel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snowflake.GuiComponents
+{
+    
+    public class ExpanderPanel : IGuiComponent
+    {
+        private bool expanded = true;
+        private float _height;
+        private float _width;
+    }
+}

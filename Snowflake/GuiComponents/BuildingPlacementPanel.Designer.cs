4981b012219bea6ec93b0b63001aa76737860e97
U-0x\Andy <Andy@0x.(none)>
2014-02-21 19:05:27 -0800

    Created class for building placement panel

---
+++ Snowflake/GuiComponents/BuildingPlacementPanel.Designer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Miyagi.Common;
+using Miyagi.Common.Data;
+using Miyagi.Common.Events;
+using Miyagi.UI;
+using Miyagi.UI.Controls;
+using Miyagi.UI.Controls.Styles;
+
+using Snowflake.Modules;
+
+namespace Snowflake.GuiComponents
+{
+    public partial class BuildingPlacementPanel
+    {
+        private Panel ParentPanel;
+        private PictureButton RotateLeft;
+        private PictureButton RotateRight;
+
+        public void CreateGui(GUI gui) {
+            int gw = gui.MiyagiSystem.RenderManager.MainViewport.Size.Width;
+            int gh = gui.MiyagiSystem.RenderManager.MainViewport.Size.Height;
+
+            ParentPanel = new Panel("BPP_Parent")
+            {
+
+            };
+        }
+    }
+}
---
+++ Snowflake/GuiComponents/BuildingPlacementPanel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snowflake.GuiComponents
+{
+    public partial class BuildingPlacementPanel : IGuiComponent
+    {
+        public void Initialize()
+        {
+
+        }
+
+        public void Update(float frametime)
+        {
+
+        }
+
+        public void Dispose() { }
+    }
+}

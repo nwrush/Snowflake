b78e6d37e1e109ed0ba50f5c959741ed070c2961
nikko.rush <nikko.rush@gmail.com>
2014-03-25 11:56:48 -0700

    Moved from plots to links

---
+++ Haswell/Links.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Haswell {
+    abstract class Links {
+        Plot start;
+        Plot end;
+        ResourceType resource;
+        float spd;
+
+        public Links(Plot _start, Plot _end,ResourceType _resource,float _speed) {
+            this.start = _start;
+            this.end = _end;
+            this.resource = _resource;
+            this.spd = _speed;
+        }
+    }
+}

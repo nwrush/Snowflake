74e33857f7eda7c7fdc83c562284b9587a8b1853
Chronister <andrewLchronister@gmail.com>
2014-04-06 15:39:08 -0700

    Dlls and fix for uints

--- Bradbury/CityWorld.cs
+++ Bradbury/CityWorld.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
+using Bradbury.Properties;
 using SharpGLass;
 using Haswell;
 
@@ -14,7 +16,8 @@ namespace Bradbury
         public override void Load()
         {
             base.Load();
-            Level l = new Level(UInt32.MaxValue, UInt32.MinValue);
+            Level l = new Level(1024, 1024);
+            l.LoadTiles(XDocument.Parse(Resources.Tileset));
             this.LoadLevel(l);
         }
     }

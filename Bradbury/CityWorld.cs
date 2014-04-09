71c3a3823c130b87ec34e51358cf1f304f1c3ea6
chronister <andrewLchronister@gmail.com>
2014-04-08 20:01:50 -0700

    fix things with grids

--- Bradbury/CityWorld.cs
+++ Bradbury/CityWorld.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using System.Drawing;
 
 using Bradbury.Properties;
 using SharpGLass;
@@ -21,8 +22,6 @@ namespace Bradbury
             l.Tilesets[0].Texture = new Texture(Resources.Tiles_gfx);
             this.LoadLevel(l);
 
-            this.Add(new Entity(new System.Drawing.PointF(-200, -200), new System.Drawing.Size(128, 128), new Texture(Resources.Tiles_gfx)));
-
             this.BackgroundColor = System.Drawing.Color.DarkGreen;
 
             Camera.X = 512 * this.Level.TileWidth;
@@ -32,14 +31,43 @@ namespace Bradbury
         public override void Update(OpenTK.FrameEventArgs e)
         {
             base.Update(e);
+
+            UpdateInput(e);
+        }
+
+        private void UpdateInput(OpenTK.FrameEventArgs e)
+        {
             if (Input.GetMouseButton(OpenTK.Input.MouseButton.Middle) == Input.KeyState.Down)
             {
-                this.Camera.X -= Input.MouseDeltaPosition.X *this.Camera.Zoom;
-                this.Camera.Y -= Input.MouseDeltaPosition.Y *this.Camera.Zoom;
+                this.Camera.X -= Input.MouseDeltaPosition.X * this.Camera.Zoom;
+                this.Camera.Y -= Input.MouseDeltaPosition.Y * this.Camera.Zoom;
             }
-            
-            this.Camera.Zoom *= (float)Math.Pow(1.1, -Input.MouseDeltaScroll);
-            
+
+            this.Camera.Zoom *= (float)Math.Pow(1.0717734625362931642130063250233 /* 10th root of 2 */, -Input.MouseDeltaScroll); 
+        }
+
+        public override void Render(OpenTK.FrameEventArgs e, Engine n)
+        {
+ 	         base.Render(e, n);
+
+             DrawGrid(e, n);
+        }
+
+        private void DrawGrid(OpenTK.FrameEventArgs e, Engine n)
+        {
+            int minx = (int)((Camera.X - Camera.HalfWidth * Camera.Zoom) / Level.TileWidth);
+            int maxx = (int)((Camera.X + Camera.HalfWidth * Camera.Zoom) / Level.TileWidth) + 1;
+            int miny = (int)((Camera.Y - Camera.HalfHeight * Camera.Zoom) / Level.TileHeight);
+            int maxy = (int)((Camera.Y + Camera.HalfHeight * Camera.Zoom) / Level.TileHeight) + 1;
+
+            for (int x = minx; x < maxx; ++x)
+            {
+                n.DrawLine(new PointF(x * Level.TileWidth, miny * Level.TileHeight), new PointF(x * Level.TileWidth, maxy * Level.TileHeight), Color.FromArgb(64, 0, 0, 0), 2);
+            }
+            for (int y = miny; y < maxy; ++y)
+            {
+                n.DrawLine(new PointF(minx * Level.TileWidth, y * Level.TileHeight), new PointF(maxx * Level.TileWidth, y * Level.TileHeight), Color.FromArgb(64, 0, 0, 0), 2);
+            }
         }
     }
 }

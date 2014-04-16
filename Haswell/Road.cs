175a8183301c9f1fec4514384a5d77d0b9fc325e
nikko.rush <nikko.rush@gmail.com>
2014-04-16 11:58:41 -0700

    Added road constructors and connections

--- Haswell/Road.cs
+++ Haswell/Road.cs
@@ -4,7 +4,18 @@ using System.Linq;
 using System.Text;
 
 namespace Haswell {
-    class Road {
+    class Road:Buildings.Infrastructure {
+        public Road[] connections;
+        int speed;//In mph?
 
+        public Road()
+            : base() {
+                this.connections = new Road[8];
+                this.speed = 35;
+        }
+        public Road(int _speed)
+            : this() {
+                this.speed = _speed;
+        }
     }
 }

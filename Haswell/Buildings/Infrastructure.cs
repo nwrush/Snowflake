d475b8ebaca89d57ee5cdba8e6dcdc8733c29d20
Nikko <nikko.rush@gmail.com>
2014-04-15 19:06:11 -0700

    Removed Comments

--- Haswell/Buildings/Infrastructure.cs
+++ Haswell/Buildings/Infrastructure.cs
@@ -4,47 +4,29 @@ using System.Linq;
 using System.Text;
 
 namespace Haswell.Buildings {
-    /// <summary>
-    /// Class Infrastructure.
-    /// </summary>
     public class Infrastructure : Building {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Infrastructure"/> class.
-        /// </summary>
         public Infrastructure()
             : base(Zones.Infrastructure) {
 
         }
 
-        /// <summary>
-        /// Updates the specified plot resources.
-        /// </summary>
-        /// <param name="plotResources">The plot resources.</param>
-        public override void Update(ResourceDict plotResources) { base.Update(plotResources); }
-        /// <summary>
-        /// Updates the hour.
-        /// </summary>
-        /// <param name="plotResources">The plot resources.</param>
-        public override void UpdateHour(ResourceDict plotResources) { base.UpdateHour(plotResources); }
-        /// <summary>
-        /// Updates the daily.
-        /// </summary>
-        /// <param name="plotResources">The plot resources.</param>
-        public override void UpdateDaily(ResourceDict plotResources) { base.UpdateHour(plotResources); }
-        /// <summary>
-        /// Updates the weekly.
-        /// </summary>
-        /// <param name="plotResources">The plot resources.</param>
-        public override void UpdateWeekly(ResourceDict plotResources) { base.UpdateHour(plotResources); }
-        /// <summary>
-        /// Updates the monthly.
-        /// </summary>
-        /// <param name="plotResources">The plot resources.</param>
-        public override void UpdateMonthly(ResourceDict plotResources) { base.UpdateHour(plotResources); }
-        /// <summary>
-        /// Updates the yearly.
-        /// </summary>
-        /// <param name="plotResources">The plot resources.</param>
-        public override void UpdateYearly(ResourceDict plotResources) { base.UpdateHour(plotResources); }
+        public override void Update(ResourceDict plotResources) {
+            base.Update(plotResources);
+        }
+        public override void UpdateHour(ResourceDict plotResources) {
+            base.UpdateHour(plotResources);
+        }
+        public override void UpdateDaily(ResourceDict plotResources) {
+            base.UpdateHour(plotResources);
+        }
+        public override void UpdateWeekly(ResourceDict plotResources) {
+            base.UpdateHour(plotResources);
+        }
+        public override void UpdateMonthly(ResourceDict plotResources) {
+            base.UpdateHour(plotResources);
+        }
+        public override void UpdateYearly(ResourceDict plotResources) {
+            base.UpdateHour(plotResources);
+        }
     }
 }

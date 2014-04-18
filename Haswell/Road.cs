567a6f21e37ecc053902f5643e1e18273832db84
Nikko <nikko.rush@gmail.com>
2014-04-17 22:13:35 -0700

    Road connections

--- Haswell/Road.cs
+++ Haswell/Road.cs
@@ -21,9 +21,17 @@ namespace Haswell {
         public void GetConnections() {
             InfiniteGrid grid = Haswell.Controller.City.Grid;
             Point loc = new Point(this.Parent.X, this.Parent.Y);
-
-            if (this.GetType() == grid.ElementAt(loc.X + 1, loc.Y).GetType()) {
-                //this.connections[0] = grid.ElementAt(loc.X + 1, loc.Y);
+            if (this.GetType() == grid.ElementAt(loc.X + 1, loc.Y).Building.GetType()) {//North
+                this.connections[0] = (Road)grid.ElementAt(loc.X + 1, loc.Y).Building;
+            }
+            if (this.GetType() == grid.ElementAt(loc.X, loc.Y + 1).Building.GetType()) {//East
+                this.connections[1] = (Road)grid.ElementAt(loc.X, loc.Y + 1).Building;
+            }
+            if (this.GetType() == grid.ElementAt(loc.X - 1, loc.Y).Building.GetType()) {//South
+                this.connections[2] = (Road)grid.ElementAt(loc.X - 1, loc.Y).Building;
+            }
+            if (this.GetType() == grid.ElementAt(loc.X, loc.Y - 1).Building.GetType()) {//West
+                this.connections[3] = (Road)grid.ElementAt(loc.X, loc.Y - 1).Building;
             }
         }
     }

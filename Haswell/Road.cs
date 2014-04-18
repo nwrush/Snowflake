ccfde3f162de077df491641d542c59cdd1e0ca81
nikko.rush <nikko.rush@gmail.com>
2014-04-17 18:09:35 -0700

    Stuff

--- Haswell/Road.cs
+++ Haswell/Road.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace Haswell {
-    class Road:Buildings.Infrastructure {
+    class Road : Buildings.Infrastructure {
         public Road[] connections;
         int speed;//In mph?
 
         public Road()
             : base() {
-                this.connections = new Road[8];
-                this.speed = 35;
+            this.connections = new Road[4];
+            this.speed = 35;
         }
         public Road(int _speed)
             : this() {
-                this.speed = _speed;
+            this.speed = _speed;
+        }
+        public void GetConnections() {
+            InfiniteGrid grid = Haswell.Controller.City.Grid;
+            Point loc = new Point(this.Parent.X, this.Parent.Y);
+
+            if (this.GetType() == grid.ElementAt(loc.X + 1, loc.Y).GetType()) {
+                this.connections[0] = grid.ElementAt(loc.X + 1, loc.Y);
+            }
         }
     }
 }

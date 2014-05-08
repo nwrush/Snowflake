17d7e4d26e702ecfa34c6f4d7714516ecd08b3c1
chronister <andrewLchronister@gmail.com>
2014-05-07 22:23:03 -0700

    Grid panel start

---
+++ Snowflake/GuiComponents/Styles/GridLayoutStyle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Miyagi.Common.Resources;
+using Miyagi.Common.Data;
+
+namespace Snowflake.GuiComponents.Styles
+{
+    public class GridLayoutStyle
+    {
+        Skin CellSkin;
+        Thickness CellMargin;
+        Thickness CellPadding;
+        Thickness CellBorder;
+        bool AllowCellResize = true;
+
+    }
+
+    public enum GridLayoutSizeMode
+    {
+        AutoFlow,
+        FixedRows,
+        FixedColumns,
+        Fixed
+    }
+}
---
+++ Snowflake/GuiComponents/Windows/GridLayoutPanel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Miyagi.UI.Controls;
+
+namespace Snowflake.GuiComponents.Windows
+{
+    public class GridLayoutPanel : Panel
+    {
+        private int _rows;
+        private int _cols;
+
+        public GridLayoutPanel()
+            : base() {
+            create();
+        }
+        public GridLayoutPanel(string name) : base(name) {
+            create();
+        }
+        private void create()
+        {
+
+        }
+
+        protected override void OnControlAdded(Miyagi.Common.Events.ValueEventArgs<Control> e)
+        {
+            base.OnControlAdded(e);
+        }
+
+        protected override void OnControlRemoved(Miyagi.Common.Events.ValueEventArgs<Control> e)
+        {
+            base.OnControlRemoved(e);
+        }
+
+        protected override void OnLayout(Miyagi.UI.Controls.Layout.LayoutEventArgs e)
+        {
+            base.OnLayout(e);
+            
+        }
+
+        public int Rows
+        {
+            get { return _rows; }
+        }
+
+        public int Columns
+        {
+            get { return _cols; }
+        }
+    }
+}

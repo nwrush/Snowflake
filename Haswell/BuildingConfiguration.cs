fd35522f9de00791d6cf642eaab6e167c02cb7d7
Nikko <nikko.rush@gmail.com>
2014-05-16 16:32:11 -0700

    Fixed == in Building config

--- Haswell/BuildingConfiguration.cs
+++ Haswell/BuildingConfiguration.cs
@@ -49,6 +49,9 @@ namespace Haswell
         }
         public static bool operator ==(BuildingConfiguration b1, BuildingConfiguration b2)
         {
+            if (b1 == null || b2 == null)
+                return false;
+
             if (b1.buildingType == b2.buildingType && b1.version == b2.version)
             {
                 return true;
